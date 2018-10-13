86308b2f0b114b0d4f7e35ed62ad8ccbf56e3fda
Yova <10067268+yovadev@users.noreply.github.com>
2018-10-13 10:45:43 -0600

    Initial commit

---
+++ src/metodoconparametros/Metodos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodoconparametros;
+
+/**
+ *
+ * @author Yova
+ */
+public class Metodos {
+    
+    public void mimensaje(String nombre){
+        System.out.println("Tu nombre es: " + nombre);
+    
+    }
+    
+}
